296a6fd4c36c986e0dd30851480501c06117c24e
mkunaparaju <mkunaparaju@gmail.com>
2016-12-01 18:08:52 -0500

    added data manager

--- src/Storage/DataManager.java
+++ src/Storage/DataManager.java
@@ -3,7 +3,12 @@
  */
 package Storage;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import Storage.Site;
+import Storage.Site.ServerStatus;
+
 
 /**
  * @author mkunaparaju
@@ -12,11 +17,53 @@ import java.util.List;
 public class DataManager {
 	 private List<Site> siteList;
 
+	 
+	 public DataManager() 
+	 {
+	     setUp();
+	 }
+
 	 public List<Site> getSiteList() 
 	 {
 		 return siteList;
 	 }
-	
+	 
+	 /**
+	     * sets up the initial database: sites with variables and their initial value
+	     */
+	    public void setUp() {
+	        // creating variables
+	        List<Index> varList = new ArrayList<Index>();
+	        for (int i = 1; i <= 20; i++) {
+	            Index v = new Index(i, i * 10);
+	            varList.add(v);
+	        }
+	        // creating sites
+	        siteList = new ArrayList<Site>();
+	        for (int i = 1; i <= 10; i++) 
+	        {
+	            Site site = new Site(i);
+	            site.setStatus(ServerStatus.UP);
+	            siteList.add(site);
+	        }
+
+	        // adding indexs to site
+	        for (int i = 1; i <= 20; i++)
+	        {
+	            // var is odd
+	            if (i % 2 != 0) 
+	            {
+	                siteList.get(i % 10).addIndexToSite(varList.get(i - 1));
+	            } 
+	            else 
+	            { 
+	            	// add to all sites
+	                for (int j = 0; j < 10; j++) {
+	                    siteList.get(j).addIndexToSite(varList.get(i - 1));
+	                }
+	            }
+	        }
+	    }
 	public void fail(int timeStamp, int siteID)
 	{
 	
@@ -31,7 +78,10 @@ public class DataManager {
 	
 	public void recover(int siteID)
 	{
-		
-	}
-
+		  Site site = getSiteList().get(siteID - 1);
+	        if (site != null) 
+	        {
+	            site.recover();
+	        }
+	    }	
 }
